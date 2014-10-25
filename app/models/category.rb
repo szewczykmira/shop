798e4621333190a921234070ccadf83374a69e21
szewczykmira <szewczyk.mira@gmail.com>
2014-10-25 21:03:24 +0200

    Assertion added

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ActiveRecord::Base
   validates :name, uniqueness: true
   validates :name, presence: true
+  has_many :products
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ActiveRecord::Base
   validates :name, :category_id, :price, presence: true
   validates :name, uniqueness: true
+  belongs_to :category
 end
