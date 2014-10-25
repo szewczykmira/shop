c470ca4cee50426bf56eb8a182f605bc4d69a5a1
szewczykmira <szewczyk.mira@gmail.com>
2014-10-25 21:01:53 +0200

    Validation added

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,4 @@
 class Category < ActiveRecord::Base
+  validates :name, uniqueness: true
+  validates :name, presence: true
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,4 @@
 class Product < ActiveRecord::Base
+  validates :name, :category_id, :price, presence: true
+  validates :name, uniqueness: true
 end
